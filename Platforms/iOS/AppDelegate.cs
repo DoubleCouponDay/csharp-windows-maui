4fd0128bd8e4c1b7ee73fa7c9bdbb439b3880443
sam <mail@samueljenks.me>
2024-04-12 12:15:24 +1200

    rm: unused targets

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace csharp_windows_desktop_gui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
